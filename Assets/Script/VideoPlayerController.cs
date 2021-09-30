77ef774476b6ce760719dbc95eb6867776c51b39
rmsdurla <gkgkgkgkyo@naver.com>
2021-09-30 12:25:59 +0900

    Videoplayer
    - VR VideoPlayer 샘플 scene 생성

---
+++ Assets/Script/VideoPlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class VideoPlayerController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    
+    public void Stop()
+    {
+        videoPlayer.Stop();
+
+    }
+
+    public void Run()
+    {
+        videoPlayer.Play();
+
+    }
+
+    public void Pause()
+    {
+        videoPlayer.Pause();
+    }
+}
